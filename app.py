f88ff476afd9227eb5da8fc0f203dd75d2b11d99
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-10-23 13:07:00 -0300

    V4 Ia na web

--- app.py
+++ app.py
@@ -1,51 +1,74 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
 import pandas as pd
+from sqlalchemy import create_engine
+import psycopg
 
 app = Flask(__name__)
 
 # Carregar o modelo previamente treinado
-modelo = joblib.load('pipeline.pkl')
+modelo = joblib.load('C:/Users/henriquelucareli-ieg/Desktop/Recoope/Tudo/Analise de Dados/pipeline.pkl')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    df = pd.read_excel('C:/Users/henriquelucareli-ieg/Desktop/Recoope/Tudo/Analise Exploratoria/base_empresas_reciclagem_IA.xlsx')
+
+    estado = df['Estado'].unique()
+    municipio = df['Município'].unique()
+    tipo = df['Tipo de Resíduo'].unique()
+    metodo_reciclagem = df['Método de Reciclagem'].unique()
+
+    return render_template('index.html', estado=estado.tolist(), municipio=municipio.tolist(), tipo=tipo.tolist(), metodo_reciclagem=metodo_reciclagem.tolist())
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Capturar os dados do formulário
-    razao_social = request.form.get('razao_social').upper()
-    estado = request.form.get('estado').upper()
-    municipio = request.form.get('municipio').upper()
-    ano = request.form.get('ano').upper()
+    estado = request.form.get('estado')
+    municipio = request.form.get('municipio')
+    ano = request.form.get('ano')
     tipo_residuo = request.form.get('tipo_residuo')
     metodo_reciclagem = request.form.get('metodo_reciclagem')
     quantidade = request.form.get('quantidade')
-    unidade_medida = 'kilogramas' # request.form.get('unidade_medida')
-    empresa_destinadora = request.form.get('empresa_destinadora').upper()
+    unidade_media='kilogramas'
 
     # Preparar os dados para predição
     dados_entrada = [[
-        razao_social, estado, municipio, ano, tipo_residuo,
-        metodo_reciclagem, quantidade, unidade_medida, empresa_destinadora
+        estado, municipio, ano, tipo_residuo,
+        metodo_reciclagem, quantidade,unidade_media
     ]]
 
-    colunas = [
-        'Razão Social', 'Estado', 'Município', 'Ano', 'Tipo de Resíduo',
-        'Método de Reciclagem', 'Quantidade', 'Unidade de Medida', 'Empresa Destinadora do Resíduo'
-    ]
+    colunas = ['Estado', 'Município', 'Ano', 'Tipo de Resíduo', 'Método de Reciclagem', 'Quantidade', 'Unidade de Medida']
     dados_entrada_df = pd.DataFrame(dados_entrada, columns=colunas)
 
     # Fazer a predição
     resultado = modelo.predict(dados_entrada_df)
 
-    print("Resultado da predição:", resultado)
+    dados_entrada_df['Resultado']=resultado
 
-    # Adicionar a situação cadastral ao DataFrame
-    dados_entrada_df['Situação Cadastral'] = resultado
+    print('Começando===============================================')
 
-    # Retornar o resultado da predição
+    # Conectar ao banco de dados
+    try:
+        conn = psycopg.connect("postgres://avnadmin:AVNS_DAFAJWqxMl1ba9hBbcZ@recoop-germinare-9764.h.aivencloud.com:16983/IA?sslmode=require")
+        print("Conexão estabelecida com sucesso!")
+
+        df = pd.DataFrame(dados_entrada_df)
+
+        # Configurar a conexão com o banco de dados usando SQLAlchemy
+        engine = create_engine("postgresql+psycopg://avnadmin:AVNS_DAFAJWqxMl1ba9hBbcZ@recoop-germinare-9764.h.aivencloud.com:16983/IA?sslmode=require")
+
+        # Salvar o DataFrame no banco de dados
+        df.to_sql('Respostas', con=engine, if_exists='replace', index=False)
+
+        print("DataFrame salvo com sucesso!")
+
+    except psycopg.OperationalError as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+    except Exception as e:
+        print(f"Erro ao salvar o DataFrame: {e}")
+    finally:
+        conn.close()
+
     return jsonify({'resultado': resultado[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
